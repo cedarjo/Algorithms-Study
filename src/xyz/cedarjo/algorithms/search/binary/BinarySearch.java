c565da102feba22a68055909c3a833e58ca9025c
cedarjo <xuxschn@outlook.com>
2019-09-04 00:23:18 +0800

    二分搜索

---
+++ src/xyz/cedarjo/algorithms/search/binary/BinarySearch.java
@@ -0,0 +1,88 @@
+package xyz.cedarjo.algorithms.search.binary;
+
+import java.util.Comparator;
+
+/**
+ * BinarySearch
+ */
+public class BinarySearch {
+
+    /**
+     * 递归实现二分搜索
+     * 
+     * @param <E>
+     * @param data
+     * @param e
+     * @param comparator
+     * @return
+     */
+    public <E> int searchRecursive(E[] data, E e, Comparator<E> comparator) {
+        return 0;
+    }
+
+    /**
+     * 非递归实现二分搜索
+     * 
+     * @param <E>
+     * @param data
+     * @param e
+     * @param comparator
+     * @return
+     */
+    public <E> int searchNonRecursive(E[] data, E e, Comparator<E> comparator) {
+        return 0;
+    }
+
+    /**
+     * 查找第一个等于指定值的元素索引
+     * 
+     * @param <E>
+     * @param data
+     * @param e
+     * @param comparator
+     * @return
+     */
+    public <E> int searchFirstEq(E[] data, E e, Comparator<E> comparator) {
+        return 0;
+    }
+
+    /**
+     * 查找最后一个等于指定值的元素索引
+     * 
+     * @param <E>
+     * @param data
+     * @param e
+     * @param comparator
+     * @return
+     */
+    public <E> int searchLastEq(E[] data, E e, Comparator<E> comparator) {
+        return 0;
+    }
+
+    /**
+     * 查找第一个大于等于指定值的元素索引
+     * 
+     * @param <E>
+     * @param data
+     * @param e
+     * @param comparator
+     * @return
+     */
+    public <E> int searchFirstGe(E[] data, E e, Comparator<E> comparator) {
+        return 0;
+    }
+
+    /**
+     * 查找最后一个小于等于给定值的元素索引
+     * 
+     * @param <E>
+     * @param data
+     * @param e
+     * @param comparator
+     * @return
+     */
+    public <E> int searchLastLe(E[] data, E e, Comparator<E> comparator) {
+        return 0;
+    }
+
+}
